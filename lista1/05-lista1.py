6d522f1082a5d4c08d9fd68792b0c3dd4e906c66
caioverissimo <caio.veb@gmail.com>
2016-05-07 23:58:18 -0300

    initialize a '04-lista1' file

--- lista1/05-lista1.py
+++ lista1/05-lista1.py
@@ -1,1 +1,15 @@
+### IAL002 - Primeira Lista
+### 5. Escreva um programa que leia um número inteiro N e em seguida leia N números reais, 
+###	calculando a soma de todos os valores digitados.
+
+### Program for calculate total of determined by user number of values ###
+
+print('\n');
+print("### Program for calculate total of determined by user number of values ###")
+print("### ------------------------------------------------------------------ ###")
+print('\n');
+
+
+
+
 print("Magic happens here!")
